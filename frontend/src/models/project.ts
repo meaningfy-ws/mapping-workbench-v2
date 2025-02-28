ae96bd5e6764804c0fe9178e6250d09fe907a94f
Victor Frunze <silverberserk@gmail.com>
2025-02-28 12:10:56 +0200

    MWB2-70: fix dev build for ui

--- frontend/src/models/project.ts
+++ frontend/src/models/project.ts
@@ -1,4 +1,4 @@
- export class Project {
+ export interface Project {
     title: string;
     identifier: string;
     description?: string;
@@ -6,13 +6,4 @@
     end_date: Date;
     "@type": string;
     "@id"?:string;
-
-    constructor(title: string, identifier: string, start_date: Date, end_date: Date, description?: string) {
-        this.title = title;
-        this.identifier = identifier;
-        this.start_date = start_date;
-        this.end_date = end_date;
-        this.description = description;
-        this["@type"] = 'project'
-    }
 }

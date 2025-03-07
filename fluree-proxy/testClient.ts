f3f78025291c62444166f1f60da380b5e85e4587
Rashif Ray Rahman <schiv@archlinux.org>
2025-03-07 21:41:42 +0600

    Update Fluree test client, example for data constraint policy
    - Distinct functions for testing read and writes
    - Additional write function for constraint testing
    - Separate data for each function
    - Write data with random UUID to keep inserting anew
    - Proper namespaces and prefixes via JSON-LD `@context`
    
    If you try to submit a Project with the same ID twice but with a
    different name, the constraint on the name will be violated. Fluree
    returns a HTTP 422, but if the proxy doesn't handle that, it will be a
    500.
    
    The write test will keep writing the same entity with different random
    IDs. You will see that entity on subsequent runs, not in the same run
    that both the read and write occur.
    
    The violation write will fail, as the violation is triggered with the
    same data as the policy write, which is commented out (run it
    uncommented once).
    
    A reminder that if you have errors starting Fluree with Docker, change
    the data volume name and create the ledger anew.

--- fluree-proxy/testClient.ts
+++ fluree-proxy/testClient.ts
@@ -1,14 +1,136 @@
 import fetch from "node-fetch";
+import { v4 as uuidv4 } from 'uuid';
 
-async function testFlureeProxy() {
-  const query = {
-    // from: "cryptids",
-    select: { '?s': ['*'] },
-    where: {
-      '@id': '?s',
-    },
-  };
+const query = {
+  // from: "cryptids",
+  "@context": {
+    "@vocab": "https://meaningfy.ws/mapping-workbench/",
+    "@base": "https://meaningfy.ws/data-centric-app-demo/",      
+    "mwb": "http://meaningfy.ws/mapping-workbench/",
+    "schema": "http://schema.org/",
+    "sh": "http://www.w3.org/ns/shacl#"
+  },
+  select: { '?s': ['*'] },
+  where: {
+    '@id': '?s',
+    '@type': "mwb:Project"
+  },
+};
 
+const transaction = {
+  "@context": {
+    "@vocab": "https://meaningfy.ws/mapping-workbench/",
+    "@base": "https://meaningfy.ws/data-centric-app-demo/",      
+    "mwb": "http://meaningfy.ws/mapping-workbench/",
+    "schema": "http://schema.org/",
+    "sh": "http://www.w3.org/ns/shacl#"
+  },
+  insert: {
+    "@id": uuidv4(),
+    "@type": "mwb:Project",
+    "schema:name": "MWB2 Periodic",
+  }
+};
+
+const transactionViolation = {
+  "@context": {
+    "@vocab": "https://meaningfy.ws/mapping-workbench/",
+    "@base": "https://meaningfy.ws/data-centric-app-demo/",      
+    "mwb": "http://meaningfy.ws/mapping-workbench/",
+    "schema": "http://schema.org/",
+    "sh": "http://www.w3.org/ns/shacl#"
+  },
+  insert: {
+    "@id": "mwb1",
+    "@type": "mwb:Project",
+    "schema:name": "MWB1.2",
+  }
+};
+
+const policy = {
+  "@context": {
+    "@vocab": "https://meaningfy.ws/mapping-workbench/",
+    "@base": "https://meaningfy.ws/data-centric-app-demo/",      
+    "mwb": "http://meaningfy.ws/mapping-workbench/",
+    "schema": "http://schema.org/",
+    "sh": "http://www.w3.org/ns/shacl#"
+  },
+  "insert": [
+      {
+        "@id": "ex:ProjectShape",
+        "@type": ["sh:NodeShape"],
+        "sh:targetClass": { "@id": "mwb:Project" },
+        "sh:property": [
+          {
+            "sh:path": { "@id": "schema:name" },
+            "sh:maxCount": 1
+          }
+        ]
+      },
+      {
+        "@id": "mwb1",
+        "@type": "mwb:Project",
+        "schema:name": "MWB1",
+        "schema:description": "This is an eForms Project"
+      }
+  ]
+}
+
+async function testFlureeProxyWritePolicy() {
+  try {
+    const response = await fetch("http://localhost:3000/transact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(policy, null, 2)
+    });
+
+    const data = await response.json();
+
+    console.log("Response from Fluree Proxy for WritePolicy:", data);
+  } catch (error) {
+    console.error("Error testing Fluree Proxy:", error);
+  }
+}
+
+async function testFlureeProxyWriteData() {
+  try {
+    const response = await fetch("http://localhost:3000/transact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(transaction, null, 2)
+    });
+
+    const data = await response.json();
+
+    console.log("Response from Fluree Proxy for WriteData:", data);
+  } catch (error) {
+    console.error("Error testing Fluree Proxy:", error);
+  }
+}
+
+async function testFlureeProxyWriteViolation() {
+  try {
+    const response = await fetch("http://localhost:3000/transact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(transactionViolation, null, 2)
+    });
+
+    const data = await response.json();
+
+    console.log("Response from Fluree Proxy for WriteViolation:", data);
+  } catch (error) {
+    console.error("Error testing Fluree Proxy:", error);
+  }
+}
+
+async function testFlureeProxyReadData() {
   try {
     const response = await fetch("http://localhost:3000/query", {
       method: "POST",
@@ -19,10 +141,14 @@ async function testFlureeProxy() {
     });
 
     const data = await response.json();
-    console.log("Response from Fluree Proxy:", data);
+
+    console.log("Response from Fluree Proxy for ReadData:", data);
   } catch (error) {
     console.error("Error testing Fluree Proxy:", error);
   }
 }
 
-testFlureeProxy();
+// testFlureeProxyWritePolicy();
+testFlureeProxyWriteData();
+testFlureeProxyReadData();
+testFlureeProxyWriteViolation();

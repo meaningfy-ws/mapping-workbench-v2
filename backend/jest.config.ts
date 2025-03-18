fe49186b9ed0647a474db7645277b7f986d469ff
Victor Frunze <silverberserk@gmail.com>
2025-03-18 11:33:34 +0200

    MWB2-70: add api unitests

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,8 +1,17 @@
-module.exports = {
-  // [...]
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json'
-    }
-  }
+export default {
+  preset: 'ts-jest/presets/default-esm', // Use the ESM preset
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1', // Map .js imports to .ts files
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true, // Enable ESM support in ts-jest
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'], // Treat .ts files as ESM
 };
--- backend/src/__tests__/app.test.ts
+++ backend/src/__tests__/app.test.ts
@@ -2,16 +2,71 @@ import request from "supertest";
 
 import app from "../app.ts";
 
-describe('Authentication',() => {
-  test('login',async () => {
-    const res = await request(app).post("/api/login");
+let bearer = ""
 
-  })
+describe('Authentication', () => {
+
+    test("Authorization middleware", async () => {
+        const res = await request(app).post("/api/get").set('Authorization', "bearer")
+        expect(res.status).toBe(403);
+    });
+
+    test('login', async () => {
+        const res = await request(app).post("/api/login").send({email: "admin@mw.com", password: '1234567'});
+        expect(res.status).toEqual(200)
+        bearer = res.body
+    })
+
+    test('me', async () => {
+        const res = await request(app).post("/api/me").send({'accessToken': bearer});
+        expect(res.body.email).toEqual("admin@mw.com")
+    })
+
+
 })
 
-describe("Test app.ts", () => {
-  test("Catch-all route", async () => {
-    const res = await request(app).post("/api/post");
-    expect(res.body).toEqual({ message: "Allo! Catch-all route." });
-  });
+describe("Test fluree", () => {
+
+    test("Test post", async () => {
+        const res = await request(app).post("/api/post").set('Authorization', bearer).send({
+            insert: {
+                title: 'test_proj',
+                identifier: 123,
+                '@type': 'projects',
+                start_date: new Date,
+                end_date: new Date
+            }
+        });
+        expect(res.status).toBe(200);
+    })
+
+
+    let projId = ""
+
+    test("Test get", async () => {
+        const res = await request(app).post("/api/get").set('Authorization', bearer).send({
+            "select": {"?s": ["@id", "title", "identifier", "description", "start_date", "end_date"]},
+            "where": {"@id": "?s", "@type": "projects"}
+        });
+        expect(res.status).toBe(200);
+        projId = res.body.find((e: { title: string; }) => e.title === 'test_proj')['@id']
+    });
+
+    test("Test put", async () => {
+        const res = await request(app).post("/api/post").set('Authorization', bearer).send({
+            insert: {
+                '@id': projId,
+                title: 'test_proj1',
+            }
+        });
+        expect(res.status).toBe(200);
+    })
+
+
+    test("Test delete", async () => {
+        const res = await request(app).delete("/api/delete").set('Authorization', bearer).send(projId);
+        expect(res.status).toBe(200);
+    })
+
+
 });
